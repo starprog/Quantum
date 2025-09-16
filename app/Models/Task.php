0491d746439be02a5f54efae5a1cefd58388ea59
rgwal001 <92636394+rgwal001@users.noreply.github.com>
2025-09-16 16:54:45 -0400

    Bug fixes. Primitive but functional To-Do List.

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name'];
 }
